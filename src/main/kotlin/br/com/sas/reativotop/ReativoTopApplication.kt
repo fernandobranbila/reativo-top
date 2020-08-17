710159bb8a1be2a663a301111c949731e267d700
Fernando <fernandobjunior@sas.com.br>
2020-08-17 12:43:01 -0300

    initial

---
+++ src/main/kotlin/br/com/sas/reativotop/ReativoTopApplication.kt
@@ -0,0 +1,11 @@
+package br.com.sas.reativotop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReativoTopApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReativoTopApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/sas/reativotop/ReativoTopApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.sas.reativotop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReativoTopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
